ab65317e531a063dea905287b3c1da53a8e4ece9
Madan-ghodechor <madanghodechor6232@gmail.com>
2024-09-29 15:47:10 +0530

    new change

--- heat_on_mouse/main.js
+++ heat_on_mouse/main.js
@@ -16,3 +16,20 @@ bodyEl.addEventListener("mousemove", (evt) => {
         spanEL.remove()
     }, 7000);
 })
+
+bodyEl.addEventListener("touchmove", (evt) => {
+    const xPos = evt.offsetX;
+    const yPos = evt.offsetY;
+
+    const spanEL = document.createElement("span");
+    spanEL.style.left = xPos + "px";
+    spanEL.style.top = yPos + "px";
+
+    const size = Math.random() * 100;
+    spanEL.style.width = size + 'px'
+    spanEL.style.height = size + 'px'
+    bodyEl.appendChild(spanEL)
+    setTimeout(() => {
+        spanEL.remove()
+    }, 7000);
+})
